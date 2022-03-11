367d52a08af40455342c87badf32fcf5473bcc36
ImranHossain5722 <imd572258@gmail.com>
2022-03-11 10:06:13 +0600

    like button is work now and like Tab is work properly

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-   likedPostsId.plus(id); 
+   likedPostsId.push(id); 
     showPosts(posts);
 };
 
@@ -147,6 +147,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById( "liked" ).innerHTML=""
     const likedPosts = getLikedPosts();
     likedPosts.forEach((post) => {
         const div = createPost(post);
