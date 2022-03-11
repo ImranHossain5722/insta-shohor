5faeb1a4f379f2f4bd49e38fcceba61e5a8a214f
ImranHossain5722 <imd572258@gmail.com>
2022-03-11 10:12:42 +0600

    report tab work now properly

--- js/app.js
+++ js/app.js
@@ -157,7 +157,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
